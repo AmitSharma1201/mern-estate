ec62902bbbb3a3099ba48b0258cb02bad8d302c8
Amit Sharma <attriamit830@gmail.com>
2024-04-27 09:40:50 +0530

    add delete user functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique:true,
     },
+
+    avatar:{
+        type: String,
+        default: "https://lh3.googleusercontent.com/a/ACg8ocI4wjVWgeP_b3VR4tByXUq3C_TyfW3IoT_t4o2ku7muDYY0fJA=s96-c"
+    },
 },{timestamps:true});
 
 const User = mongoose.model('User',userSchema)
